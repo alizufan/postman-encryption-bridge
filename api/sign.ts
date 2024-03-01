19c23de02999d10b75331d1a81fe366596ac4c17
X Boy <muhammadalizufan@gmail.com>
2024-03-01 16:14:28 +0700

    add: sign

--- api/sign.ts
+++ api/sign.ts
@@ -2,9 +2,18 @@ import type { VercelRequest, VercelResponse } from '@vercel/node'
 import { privateEncrypt } from 'crypto'
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  const { data, privateKey } = req.body
+  const { data = '', privateKey = '' } = req.body
+
+  let resp;
+  try {
+    resp = privateEncrypt(privateKey, Buffer.from(data)).toString("hex")
+  } catch (error) {
+    return res.json({
+      error,
+    })
+  }
   
   return res.json({
-    signature: privateEncrypt(privateKey, Buffer.from(data)).toString("hex"),
+    signature: resp,
   })
 }
