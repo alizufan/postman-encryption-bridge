0ed8d667c3db795216fea8d5b1f59071809c1200
X Boy <muhammadalizufan@gmail.com>
2024-03-01 16:58:30 +0700

    fix

--- api/sign.ts
+++ api/sign.ts
@@ -2,6 +2,26 @@ import type { VercelRequest, VercelResponse } from '@vercel/node'
 import { privateEncrypt } from 'crypto'
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
+  function unescape(str) {
+    return (str + '==='.slice((str.length + 3) % 4))
+      .replace(/-/g, '+')
+      .replace(/_/g, '/')
+  }
+  
+  function escape(str) {
+    return str.replace(/\+/g, '-')
+      .replace(/\//g, '_')
+      .replace(/=/g, '')
+  }
+  
+  const encode = (str: string) => {
+    return escape(Buffer.from(str, 'utf8').toString('base64'))
+  }
+  
+  const decode = (str: string) => {
+    return Buffer.from(unescape(str), 'base64').toString('utf8')
+  }
+  
   let { data, privateKey } = req.query
   
   if (Array.isArray(data)) {
@@ -11,16 +31,19 @@ export default function handler(req: VercelRequest, res: VercelResponse) {
   if (Array.isArray(privateKey)) {
     privateKey = privateKey[0]
   }
+
   console.log("DATA: ", data, privateKey)
+  privateKey = decode(privateKey)
 
   let hash = '';
   try {
     hash = privateEncrypt(privateKey.split(String.raw`\n`).join('\n'), Buffer.from(data)).toString("hex")
   } catch (error) {
-    return res.json({
+    return res.status(500).json({
       error,
     })
   }
+
   console.log("HASH-DATA: ", hash)
   
   return res.json({
